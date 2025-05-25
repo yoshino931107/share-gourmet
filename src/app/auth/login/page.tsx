a225a684ed7c397ea4c78ebbeb989b263c0dfda2
yoshino <kano931107@gmail.com>
2025-05-25 13:57:42 +0900

    ⚡️ Headerつけ忘れ

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-
 import { LogIn } from "@/components/ui/LogIn";
+import Header from "@/components/ui/Header";
 
 const LoginPage = () => {
   return (
     <div className="flex min-h-screen flex-col">
+      <Header />
       <LogIn />
     </div>
   );
