ebd374993501b6d1e0b32a956f4c7517c5f3d104
yoshino <kano931107@gmail.com>
2025-06-03 12:20:15 +0900

    ✨ headファイル、OGP画像追加

---
+++ src/app/head.tsx
@@ -0,0 +1,19 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        property="og:title"
+        content="シェアグル - 気になるグルメ、シェア＆ストック！"
+      />
+      <meta
+        property="og:description"
+        content="「シェアグル」は、気になる飲食店を家族や友人、恋人、同僚とシェア＆ストックできるアプリです。外食する際の「どこ行こう？」問題を、スムーズに楽しく解決します。"
+      />
+      <meta
+        property="og:image"
+        content="https://share-gourmet.vercel.app/OGP.png"
+      />
+    </>
+  );
+}
